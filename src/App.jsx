b52c708dfddabc402869da74385bf46c2b508b2d
Sewanu Isaiah <sewanu708@gmail.com>
2025-06-03 20:12:52 +0100

    Formalities

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header"
+import Hero from "./components/Hero"
+import MiniHeader from "./components/MiniHeader"
+
+function App() {
+
+  return (
+    <>
+      <MiniHeader />
+      <Header />
+      <Hero />
+    </>
+  )
+}
+
+export default App
